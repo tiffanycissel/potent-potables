f6014438240dee822afffec29416b3c71c0f6052
Tiffany Cissel <tiffany.cissel@gmail.com>
2019-05-02 17:38:30 -0400

    Updates

---
+++ precache-manifest.019d9692dcb52ef5555c6a6a1b3975a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddcfd0aa0cf941eb5d18a27bad90dbe",
+    "url": "/potent-potables/index.html"
+  },
+  {
+    "revision": "b0631f131bc2f3b41282",
+    "url": "/potent-potables/static/css/main.2dead4c8.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/potent-potables/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "b0631f131bc2f3b41282",
+    "url": "/potent-potables/static/js/main.d5d0c0b5.chunk.js"
+  },
+  {
+    "revision": "1b3d2ec873c90cb99ff3",
+    "url": "/potent-potables/static/js/runtime~main.1e71a9df.js"
+  }
+]);
